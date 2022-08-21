11966245272e6434aa9ccbba88d303f4196b8d61
Jimmy Andreas <jimmysignature@gmail.com>
2022-08-20 20:50:20 -0400

    Completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,21 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    #/students?name=kirby
+    students = if params[:name]
+                 Student.by_name(params[:name])
+               else
+                 Student.all
+               end    
     render json: students
   end
+
+  def show
+    students = Student.find_by(id: params[:id])
+    render json: students
+  end
+
+
+
 
 end
